bd245e04094115e9efbeb8c0a92d3789576fcab5
clstr co <s@clstr.co>
2020-06-10 15:00:39 -0700

    fixes pathology

--- gbdfail.go
+++ gbdfail.go
@@ -3,8 +3,5 @@ package gbdfail
 import "net/http"
 
 func Routes(mux *http.ServeMux) {
-	// just like the example at
-	// https://github.com/go-bindata/go-bindata#serve-assets-with-nethttp we
-	// leave the trailing slash off:
-	mux.Handle("/s", http.FileServer(AssetFile()))
+	mux.Handle("/s/", http.StripPrefix("/s/", http.FileServer(AssetFile())))
 }
